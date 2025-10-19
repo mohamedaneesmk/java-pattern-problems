033546326e3f1cd9123e2a37890327fbfeb9d37b
mohamedaneesmk <21ec267@students.stjosephs.ac.in>
2025-10-19 13:15:27 +0530

    File added

---
+++ pattern04.java
@@ -0,0 +1,12 @@
+public class pattern04 {
+    public static void main(String[] args) {
+        int num = 5;
+        
+        for(int row=1;row<=num;row++){
+            for(int col=1;col<=row;col++){
+                System.out.print(col+" ");
+            }
+            System.out.println();
+        }
+    }
+}
