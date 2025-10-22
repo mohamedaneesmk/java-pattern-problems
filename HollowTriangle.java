007d3db92be0efe96d140fb8413a65057e8aca62
mohamedaneesmk <21ec267@students.stjosephs.ac.in>
2025-10-22 19:03:47 +0530

    File added

---
+++ HollowTriangle.java
@@ -0,0 +1,21 @@
+public class HollowTriangle {
+    public static void main(String[] args) {
+        int num = 5;
+
+        for (int row = 1; row <= num; row++) {
+            for (int spaces = num - row; spaces > 0; spaces--) {
+                System.out.print(" ");
+            }
+
+            for (int col = 1; col <= row; col++) {
+                if (col == 1 || col == row || row == num)
+                    System.out.print("* ");
+                else
+                    System.out.print("  ");
+            }
+            System.out.println();
+
+        }
+
+    }
+}
