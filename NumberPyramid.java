7d26c27a7e3fd71c67eefc4a1cc36115a2f9f1ea
mohamedaneesmk <21ec267@students.stjosephs.ac.in>
2025-10-25 22:30:57 +0530

    Number Pyramid

---
+++ NumberPyramid.java
@@ -0,0 +1,39 @@
+public class NumberPyramid {
+    public static void main(String[] args) {
+        int num = 8; 
+
+        // Top half including the center
+        for (int i = 1; i <= num; i++) {
+            // Print leading spaces
+            for (int spaces = 0; spaces < (num - i); spaces++) {
+                System.out.print("  ");
+            }
+            // Print descending numbers
+            for (int j = i; j >= 1; j--) {
+                System.out.print(j + " ");
+            }
+            // Print ascending numbers
+            for (int j = 2; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+
+        // Bottom half (excluding center)
+        for (int i = num - 1; i >= 1; i--) {
+            // Print leading spaces
+            for (int spaces = 0; spaces < (num - i); spaces++) {
+                System.out.print("  ");
+            }
+            // Print descending numbers
+            for (int j = i; j >= 1; j--) {
+                System.out.print(j + " ");
+            }
+            // Print ascending numbers
+            for (int j = 2; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+}
