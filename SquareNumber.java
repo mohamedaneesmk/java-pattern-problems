dcaec485fcccbf64463cf8e1ad8f2fb207555cf3
mohamedaneesmk <21ec267@students.stjosephs.ac.in>
2025-10-26 22:18:54 +0530

    Square Number

--- SquareNumber.java
+++ SquareNumber.java
@@ -1,21 +1,20 @@
 public class SquareNumber {
     public static void main(String[] args) {
-        int num = 5;
-        int size = 2 * num - 1;
+        int num = 8;
+        int size = 2 * num - 1; // 5
 
-        for (int row = 0; row < size; row++) {
+        for (int row = 0; row < size; row++) { 
             for (int col = 0; col < size; col++) {
-                int top = row;
+                int top = row; 
                 int left = col;
-                int bottom = size - 1 - row;
-                int right = size - 1 - col;
-
+                int bottom = size - 1 - row; 
+                int right = size - 1 - col; 
+ 
                 int min = Math.min(Math.min(top, bottom), Math.min(left, right));
-
+            
                 System.out.print((num - min) + " ");
             }
             System.out.println();
         }
-
     }
 }
