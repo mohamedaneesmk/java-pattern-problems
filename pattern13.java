61ff5a6749fbfd08b5b2aa5ace98b53216ce21da
mohamedaneesmk <21ec267@students.stjosephs.ac.in>
2025-10-26 11:12:42 +0530

    File added

--- pattern13.java
+++ pattern13.java
@@ -2,11 +2,11 @@ public class pattern13 {
     public static void main(String[] args) {
         int num = 5;
 
-        for(int row=1;row<=num;row++){
-            for(int col=1;col<=num;col++){
-                if(col==1||col==num||row==1||row==num)
+        for (int row = 1; row <= num; row++) {
+            for (int col = 1; col <= num; col++) {
+                if (col == 1 || col == num || row == 1 || row == num)
                     System.out.print("*");
-                else 
+                else
                     System.out.print(" ");
             }
             System.out.println();
